4762559adb3fe1496f64e2eaddcca2604582070e
Sean Chavez <realseanchavez@gmail.com>
2019-03-23 20:14:30 -0700

    challs 2 & 3

--- async/index.js
+++ async/index.js
@@ -9,23 +9,24 @@ function testMe() {
   console.log("Partnah");
 }
 // After thinking it through, uncomment the following line to check your guess!
- testMe(); // what order should these log out? Howdy or Partnah first?
+testMe(); // what order should these log out? Howdy or Partnah first?
 
 /* CHALLENGE 2 */
 
 function delayedGreet() {
-  // ADD CODE HERE
+  setTimeout(() => console.log("Welcome"), 3000);
 }
 // Uncomment the following line to check your work!
-// delayedGreet(); // should log (after 3 seconds): welcome
+delayedGreet(); // should log (after 3 seconds): welcome
 
 /* CHALLENGE 3 */
 
 function helloGoodbye() {
-  // ADD CODE HERE
+  setTimeout(() => console.log("good bye"), 2000);
+  console.log("hello");
 }
 // Uncomment the following line to check your work!
-// helloGoodbye(); // should log: hello // should also log (after 3 seconds): good bye
+helloGoodbye(); // should log: hello // should also log (after 3 seconds): good bye
 
 /* CHALLENGE 4 */
 
