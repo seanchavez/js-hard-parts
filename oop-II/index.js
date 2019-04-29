d6a0eaa89e37d348621565ae936b5258ddd9ac1d
Sean Chavez <realseanchavez@gmail.com>
2019-04-28 20:11:02 -0700

    shh

--- oop-II/index.js
+++ oop-II/index.js
@@ -151,7 +151,7 @@ function userFactory(name, score) {
 var adminFunctionStore = new userFunctionStore();
 
 function adminFactory(name, score) {
-  code;
+  "code here";
 }
 
 /* Put code here for a method called sharePublicMessage*/
