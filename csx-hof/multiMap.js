d176fdf016f7901464ff0d815ac999d9d3203c68
Sean Chavez <realseanchavez@gmail.com>
2019-05-01 07:43:26 -0700

    majority

--- csx-hof/multiMap.js
+++ csx-hof/multiMap.js
@@ -20,3 +20,18 @@ function repeater(str) {
 const items = ["catfood", "glue", "beer"];
 const functions = [uppercaser, capitalize, repeater];
 console.log(multiMap(items, functions)); // should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }, 'Beer', 'beerbeer'] }
+
+function majority(arr, cb) {
+  const trues = arr.filter(cb);
+  if (trues.length > arr.length / 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const isOdd = function(num) {
+  return num % 2 === 1;
+};
+console.log(majority([1, 2, 3, 4, 5], isOdd)); // should log: true
+console.log(majority([2, 3, 4, 5], isOdd)); // should log: false
