0994e6de3ab1b4599c7a7ee087e390ec72c7018b
Sean Chavez <realseanchavez@gmail.com>
2019-05-01 13:30:02 -0700

    goodKeys

--- csx-hof/groupBy.js
+++ csx-hof/groupBy.js
@@ -14,3 +14,25 @@ const floored = function(num) {
   return Math.floor(num);
 };
 console.log(groupBy(decimals, floored)); // should log: { 1: [1.3], 2: [2.1, 2.4] }
+
+function goodKeys(obj, cb) {
+  const results = [];
+  for (const prop in obj) {
+    if (cb(obj[prop])) {
+      results.push(prop);
+    }
+  }
+  return results;
+}
+
+const sunny = {
+  mac: "priest",
+  dennis: "calculator",
+  charlie: "birdlaw",
+  dee: "bird",
+  frank: "warthog"
+};
+function startsWithBird(str) {
+  return str.slice(0, 4).toLowerCase() === "bird";
+}
+console.log(goodKeys(sunny, startsWithBird)); // should log: ['charlie', 'dee']
