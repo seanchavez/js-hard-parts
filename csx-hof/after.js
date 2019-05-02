66ca02fea6251bdce1a560892e605cff90fcdfa1
Sean Chavez <realseanchavez@gmail.com>
2019-05-02 13:31:58 -0700

    delay

--- csx-hof/after.js
+++ csx-hof/after.js
@@ -16,3 +16,15 @@ const afterCalled = after(3, called);
 console.log(afterCalled("world")); // -> nothing is printed
 console.log(afterCalled("world")); // -> nothing is printed
 console.log(afterCalled("world")); // -> 'hello world' is printed
+
+function delay(cb, waitTime) {
+  return () => {
+    setTimeout(cb, waitTime);
+  };
+}
+
+let count = 0;
+const delayedFunc = delay(() => count++, 1000);
+delayedFunc();
+console.log(count); // should print '0'
+setTimeout(() => console.log(count), 1000); // should print '1' after 1 second
