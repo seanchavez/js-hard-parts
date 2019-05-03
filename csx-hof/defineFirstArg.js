721c8a7801e218625ae3cc304a23fd6a34f41098
Sean Chavez <realseanchavez@gmail.com>
2019-05-02 17:15:55 -0700

    dateStamp

--- csx-hof/defineFirstArg.js
+++ csx-hof/defineFirstArg.js
@@ -9,3 +9,14 @@ const subtract = function(big, small) {
 };
 const subFrom20 = defineFirstArg(subtract, 20);
 console.log(subFrom20(5)); // should log: 15
+
+function dateStamp(cb) {
+  return (...args) => ({
+    date: new Date().toDateString(),
+    output: cb(...args)
+  });
+}
+
+const stampedMultBy2 = dateStamp(n => n * 2);
+console.log(stampedMultBy2(4)); // should log: { date: (today's date), output: 8 }
+console.log(stampedMultBy2(6)); // should log: { date: (today's date), output: 12 }
