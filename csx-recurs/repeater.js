ea5fa9a7a1219cf45c325de65db38326fba46477
Sean Chavez <realseanchavez@gmail.com>
2019-05-04 09:52:13 -0700

    factorial

--- csx-recurs/repeater.js
+++ csx-recurs/repeater.js
@@ -11,3 +11,14 @@ function repeater(char) {
 
 console.log(repeater("g"));
 console.log(repeater("j"));
+
+function factorial(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(4)); // -> 24
+console.log(factorial(6)); // -> 720
+console.log(factorial(0)); // -> 1
