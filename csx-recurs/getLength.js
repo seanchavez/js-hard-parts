a88de70075b943f3123827615ae9c6cb88fee59d
Sean Chavez <realseanchavez@gmail.com>
2019-05-04 12:43:38 -0700

    pow

--- csx-recurs/getLength.js
+++ csx-recurs/getLength.js
@@ -11,3 +11,11 @@ console.log(getLength([1])); // -> 1
 console.log(getLength([1, 2])); // -> 2
 console.log(getLength([1, 2, 3, 4, 5])); // -> 5
 console.log(getLength([])); // -> 0
+
+function pow(base, exponent) {
+  if (exponent === 1) return base * exponent;
+  return base * pow(base, exponent - 1);
+}
+
+console.log(pow(2, 4)); // -> 16
+console.log(pow(3, 5)); // -> 243
