8923371757de84518938b8452b45b4abf2a071fd
Sean Chavez <realseanchavez@gmail.com>
2019-05-06 13:08:19 -0700

    run in order

--- csx-recurs/limitedInterval.js
+++ csx-recurs/limitedInterval.js
@@ -5,3 +5,32 @@ function limitedInterval(callback, wait, limit) {
 
 limitedInterval(() => console.log("repeating"), 100, 550);
 // should log 'repeating' once per 100 ms, five times.
+
+function runInOrder(functions, times) {
+  let delay = 0;
+  functions.forEach((element, index) => {
+    delay += times[index];
+    setTimeout(() => {
+      element();
+    }, delay);
+  });
+}
+
+function sayHi() {
+  console.log("hi");
+}
+function sayBye() {
+  console.log("bye");
+}
+function sayHowdy() {
+  console.log("howdy");
+}
+
+runInOrder([sayHi, sayBye, sayHowdy], [200, 100, 300]);
+
+/*
+should log:
+'hi' (after 200 ms)
+'bye' (100 ms after 'hi')
+'howdy' (300 ms after 'bye')
+*/
