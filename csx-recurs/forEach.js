969c1a485c768698e23eda4c85473db2673c938d
Sean Chavez <realseanchavez@gmail.com>
2019-05-06 09:26:15 -0700

    forEach?

--- csx-recurs/forEach.js
+++ csx-recurs/forEach.js
@@ -11,18 +11,3 @@ function delayLog(delayTime, i) {
 }
 
 forEach(delays, delayLog);
-
-let dataReceived;
-
-function ajaxSimulate(id, callback) {
-  const database = ["Aaron", "Barbara", "Chris"];
-  setTimeout(() => callback(database[id]), 0);
-}
-
-function storeData(data) {
-  dataReceived = data;
-  console.log(dataReceived);
-}
-
-ajaxSimulate(1, storeData);
-console.log(dataReceived);
