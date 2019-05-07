06ea6ac052c5db3d2b1e17ac330c1aa9d906b0c6
Sean Chavez <realseanchavez@gmail.com>
2019-05-07 16:21:11 -0700

    personFromPersonStore

--- csx-oop/makePerson.js
+++ csx-oop/makePerson.js
@@ -10,3 +10,23 @@ const vicky = makePerson("Vicky", 24);
 
 console.log(vicky.name); // -> Logs 'Vicky'
 console.log(vicky.age); // -> Logs 24
+
+const personStore = {
+  greet: function() {
+    console.log("hello");
+  }
+};
+
+function personFromPersonStore(name, age) {
+  // add code here
+  const person = Object.create(personStore);
+  person.name = name;
+  person.age = age;
+  return person;
+}
+
+const sandra = personFromPersonStore("Sandra", 26);
+
+console.log(sandra.name); // -> Logs 'Sandra'
+console.log(sandra.age); // -> Logs 26
+sandra.greet(); // -> Logs 'hello'
