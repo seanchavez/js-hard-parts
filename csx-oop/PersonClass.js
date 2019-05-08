c45309356540abb2be7d263252cfe70332140884
Sean Chavez <realseanchavez@gmail.com>
2019-05-07 22:23:12 -0700

    DeveloperClass

--- csx-oop/PersonClass.js
+++ csx-oop/PersonClass.js
@@ -11,3 +11,28 @@ class PersonClass {
 const george = new PersonClass("George");
 
 george.greet(); // -> Logs 'hello'
+
+class PersonClass {
+  constructor(name) {
+    this.name = name;
+  }
+
+  greet() {
+    console.log("hello");
+  }
+}
+
+class DeveloperClass extends PersonClass {
+  constructor(name) {
+    super(name);
+  }
+
+  introduce() {
+    console.log("Hello World, my name is " + this.name);
+  }
+}
+
+const thai = new DeveloperClass("Thai", 32);
+
+console.log(thai.name); // -> Logs 'Thai'
+thai.introduce(); // -> Logs 'Hello World, my name is Thai'
